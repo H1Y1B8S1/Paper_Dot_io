cd99434f23a1e3126e8e4aeda30792fb4d83ceec
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-04-21 16:11:15 +0530

    camera follow

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
